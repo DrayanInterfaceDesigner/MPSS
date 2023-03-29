ccfb8a716e890f9c921c7e91333e9926731e35cd
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2023-03-29 19:44:39 -0300

    module login

--- modules/login.py
+++ modules/login.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-_login = Blueprint("login", __name__, static_folder="../static", template_folder="../view")
+_login = Blueprint("login", __name__, static_folder="../static", template_folder="../view/login/")
 
 @_login.route("/", methods=["GET", "POST"])
 def login():
