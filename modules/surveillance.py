e7d970e23a54591f718993221a5bef44e1187f75
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2023-03-29 20:14:39 -0300

    ghfd

--- modules/surveillance.py
+++ modules/surveillance.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template
 
 _surveillance = Blueprint("surveillance", __name__, static_folder="../static", template_folder="../view")
 
+devices = [["detector_de_presença", "sinal"]]
+
 @_surveillance.route("/", methods=["GET", "POST"])
 def surveillance():
-    return render_template("surveillance.html")
+    return render_template("surveillance.html", devices=devices)
 
 @_surveillance.route("/emergency", methods=["GET", "POST"])
 def emergency():
