cb832415f775207c1ac754996d08c382b287d64a
acculloko <lucca.lucchin@gmail.com>
2023-03-29 19:41:47 -0300

    marcelo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from modules.home import _home
 from modules.register import _register
+from modules.login import _login
 from modules.surveillance import _surveillance
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 app.register_blueprint(_home)
 app.register_blueprint(_register, url_prefix="/register")
+app.register_blueprint(_login, url_prefix="/login")
 app.register_blueprint(_surveillance, url_prefix="/surveillance")
 
 if __name__ == "__main__":
---
+++ modules/login.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+_login = Blueprint("login", __name__, static_folder="../static", template_folder="../view")
+
+@_login.route("/", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
