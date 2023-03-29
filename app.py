b6f7aac37878a67e37c516b4db129412c2b6c84f
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2023-03-29 18:25:42 -0300

    register

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from modules.register import _register
 
 app = Flask(__name__)
 
 
-app.register_blueprint()
+app.register_blueprint(_register, url_prefix="/register")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
---
+++ modules/register.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, redirect, render_template
+
+_register = Blueprint("register", __name__, static_folder="../static", template_folder="../view")
+
+@_register.route("/", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+@_register.route("/create", methods=["GET"])
+def create():
+    return redirect("login.html")
