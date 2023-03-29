1d59076d6e0da32fba3897f99667406e53955aca
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2023-03-29 20:01:11 -0300

    blabla

--- modules/surveillance.py
+++ modules/surveillance.py
@@ -5,3 +5,7 @@ _surveillance = Blueprint("surveillance", __name__, static_folder="../static", t
 @_surveillance.route("/", methods=["GET", "POST"])
 def surveillance():
     return render_template("surveillance.html")
+
+@_surveillance.route("/emergency", methods=["GET", "POST"])
+def emergency():
+    return render_template("emergency.html")
